21a32d2ba230190e3e176fe1882e1bda70dd756c
Melly <mebostjancic@edu.aau.at>
2024-03-27 23:56:08 +0100

    KT: inital commit empty layout

--- app/src/main/java/com/example/frontend/MainActivity.java
+++ app/src/main/java/com/example/frontend/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.frontend;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
