9041f64a571a24b969d577c9459682fdbc56d290
Fipsi <phigattenloehner@outlook.com>
2024-03-27 21:17:58 +0100

    Issue#5: inital dice setup

---
+++ app/src/main/java/com/example/frontend/gamelogic/Dice.java
@@ -0,0 +1,35 @@
+package com.example.frontend.gamelogic;
+
+import androidx.annotation.NonNull;
+
+import java.util.Random;
+
+public class Dice {
+    private int dice;
+    private Random ran;
+
+    public Dice(){
+        super();
+        dice=0;
+        ran=new Random();
+    }
+
+    public void useDice(){
+        dice= ran.nextInt(6)+1;
+    }
+
+    //is also sum of dice
+    public int getDice() {
+        return dice;
+    }
+
+    public void setRan(Random ran){
+        this.ran = ran;
+    }
+
+    @NonNull
+    @Override
+    public String toString(){
+        return String.format("dice=&d",dice);
+    }
+}
--- app/src/main/java/com/example/frontend/ui/MainActivity.java
+++ app/src/main/java/com/example/frontend/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.frontend;
+package com.example.frontend.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
