c0d30f1d1608921732f41dbc7e65377b402025d9
Fipsi <phigattenloehner@outlook.com>
2024-03-30 11:48:35 +0100

    KT: add dice fragment

---
+++ app/src/main/java/com/example/frontend/ui/DiceFragment.java
@@ -0,0 +1,19 @@
+package com.example.frontend.ui;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+import androidx.fragment.app.Fragment;
+
+public class DiceFragment extends Fragment implements SensorEventListener {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
