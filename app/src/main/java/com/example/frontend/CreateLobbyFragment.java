10f9b4f704635bf61b16002db7d37bb701724320
melly0102 <mebostjancic@edu.aau.at>
2024-04-09 19:35:21 +0200

    KT: Added Create Lobby Navigation and redesign

--- app/src/main/java/com/example/frontend/CreateLobbyFragment.java
+++ app/src/main/java/com/example/frontend/CreateLobbyFragment.java
@@ -3,10 +3,14 @@ package com.example.frontend;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CreateLobbyFragment extends Fragment {
+    private Button createGame;
+    private Button joinGame;
 
     public CreateLobbyFragment() {
     }
@@ -30,9 +36,45 @@ public class CreateLobbyFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_create_lobby, container, false);
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view =  inflater.inflate(R.layout.fragment_create_lobby, container, false);
+            findViews(view);
+            onCreateGameCLick();
+            onJoinClick();
+            return view;
+        }
+
+        private void onCreateGameCLick() {
+            createGame.setOnClickListener(v -> showFragmentLobby());
+        }
+
+        private void onJoinClick() {
+            joinGame.setOnClickListener(v -> showGameBoardFragment());
+        }
+        private void findViews(View view){
+            createGame = view.findViewById(R.id.btn_createGame);
+            joinGame = view.findViewById(R.id.btn_joinGame);
+        }
+
+        private void showFragmentLobby(){
+            Lobby_Fragment lobbyFragment = new Lobby_Fragment();
+
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.replace(R.id.fragmentContainerView2,lobbyFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+        private void showGameBoardFragment(){
+            GameBoardFragment gameBoardFragment = new GameBoardFragment();
+
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.replace(android.R.id.content,gameBoardFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
-}
