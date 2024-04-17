6d184a0f9368ac801edac6eaee1c0877e213fb0a
melly0102 <mebostjancic@edu.aau.at>
2024-04-17 10:50:57 +0200

    fix: refactored MainAcvitiy

--- app/src/main/java/com/example/frontend/MainActivity.java
+++ app/src/main/java/com/example/frontend/MainActivity.java
@@ -29,8 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView inputUsername;
     private TextView inputAge;
     private View createLobbyFragment;
-    private View fragmentDice;
-    private View endFrag;
+    View rootView;
     private final Handler handler = new Handler(Looper.getMainLooper());
 
 
@@ -40,47 +39,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         client.start();
-
         findViews();
-
-        checkInputUsername(inputUsername);
-
-        checkInputAge(inputAge);
-        // Get the root layout of your activity
-        View rootView = findViewById(android.R.id.content);
-
-        // Set a touch listener to the root layout
-        rootView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // Hide the keyboard when touched
-                hideKeyboard(v);
-                return false;
-            }
-        });
-
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-
-
-            //Send to server the responseUser and responseAge
-
-            public void onClick(View v) {
-                final String responseUser = inputUsername.getText().toString();
-                String responesAge = inputAge.getText().toString();
-                Log.i("Change Fragement", "Fragment change event started");
-                if (createLobbyFragment.getVisibility() == View.INVISIBLE) {
-                    createLobbyFragment.setVisibility(View.GONE);
-                } else {
-                    createLobbyFragment.setVisibility(View.VISIBLE);
-                    showCreateLobbyFragment();
-
-                }
-                Client.send(new Request(CommandType.REGISTER, new RegisterPayload(responseUser, Integer.parseInt(responesAge))));
-                Log.i("ResponseAge", responesAge.toString());
-
-            }
-        });
+        setListeners();
     }
 
     void findViews() {
@@ -88,12 +48,45 @@ public class MainActivity extends AppCompatActivity {
         inputUsername = findViewById(R.id.startScreenUsername);
         inputAge = findViewById(R.id.startScreenAge);
         createLobbyFragment = findViewById(R.id.fragmentContainerView2);
-        fragmentDice = findViewById(R.id.dice2);
-        endFrag = findViewById(R.id.endfragment);
+        rootView = findViewById(android.R.id.content);
+
     }
 
-    void checkInputUsername(TextView inputUsername) {
-        inputUsername.addTextChangedListener(new TextWatcher() {
+    private void setListeners() {
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                hideKeyboard(v);
+                return false;
+            }
+        });
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onClickStartButton();
+            }
+        });
+        inputUsername.addTextChangedListener(createUserNameTextWatcher());
+        inputAge.addTextChangedListener(createAgeTextWatcher());
+    }
+
+    private void onClickStartButton() {
+        String responseUser = inputUsername.getText().toString();
+        String responesAge = inputAge.getText().toString();
+
+        Log.i("Change Fragement", "Fragment change event started");
+        if (createLobbyFragment.getVisibility() == View.INVISIBLE) {
+            createLobbyFragment.setVisibility(View.GONE);
+        } else {
+            createLobbyFragment.setVisibility(View.VISIBLE);
+            showCreateLobbyFragment();
+
+        }
+        Client.send(new Request(CommandType.REGISTER, new RegisterPayload(responseUser, Integer.parseInt(responesAge))));
+        Log.i("ResponseAge", responesAge.toString());
+
+    }
+    private TextWatcher createUserNameTextWatcher(){
+        return new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
                 // Nichts zu tun hier
@@ -112,11 +105,10 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
                 // Nichts zu tun hier
             }
-        });
+        };
     }
-
-    void checkInputAge(TextView inputAge) {
-        inputAge.addTextChangedListener(new TextWatcher() {
+    private TextWatcher createAgeTextWatcher(){
+        return new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
                 // Nichts zu tun hier
@@ -135,9 +127,8 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
                 // Nichts zu tun hier
             }
-        });
-    }
-
+    };
+}
     private boolean isValidAge(String ageTxt) {
         if (ageTxt.isEmpty()) {
             return false;
@@ -157,16 +148,12 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.fragmentContainerView2, createLobbyFragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-
-        //TODO:  maybe use switch-case statement for easier change Fragment
-
     }
     private void hideKeyboard(View view) {
 
         InputMethodManager inputMethodManager = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
-
 }
 
 
