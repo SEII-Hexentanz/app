2652eabc1b9caaa1af02456e7c3289963a15d712
melly0102 <mebostjancic@edu.aau.at>
2024-04-25 00:39:30 +0200

    KT: List of GameboardFields

--- app/src/main/java/com/example/frontend/GameBoardFragment.java
+++ app/src/main/java/com/example/frontend/GameBoardFragment.java
@@ -2,6 +2,8 @@
 package com.example.frontend;
 
 import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,11 +16,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.SortedSet;
 
 public class GameBoardFragment extends Fragment {
@@ -89,9 +95,41 @@ public class GameBoardFragment extends Fragment {
     private ImageView btnYellowGoal2;
     private ImageView btnYellowGoal3;
     private ImageView btnYellowGoal4;
-
-
-
+    private ImageView boardField0;
+    private ImageView boardField1;
+    private ImageView boardField2;
+    private ImageView boardField3;
+    private ImageView boardField4;
+    private ImageView boardField5;
+    private ImageView boardField6;
+    private ImageView boardField7;
+    private ImageView boardField8;
+    private ImageView boardField9;
+    private ImageView boardField10;
+    private ImageView boardField11;
+    private ImageView boardField12;
+    private ImageView boardField13;
+    private ImageView boardField14;
+    private ImageView boardField15;
+    private ImageView boardField16;
+    private ImageView boardField17;
+    private ImageView boardField18;
+    private ImageView boardField19;
+    private ImageView boardField20;
+    private ImageView boardField21;
+    private ImageView boardField22;
+    private ImageView boardField23;
+    private ImageView boardField24;
+    private ImageView boardField25;
+    private ImageView boardField26;
+    private ImageView boardField27;
+    private ImageView boardField28;
+    private ImageView boardField29;
+    private ImageView boardField30;
+    private ImageView boardField31;
+    private ImageView boardField32;
+    private ImageView boardField33;
+    private ImageView boardField34;
     private ScaleGestureDetector scaleGestureDetector;
 
     public GameBoardFragment() {
@@ -132,12 +170,15 @@ public class GameBoardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_game_board, container, false);
-
+        List<ImageView> imageViews = new ArrayList<>();
         findViews(view);
         setGameBoardUsername();
         onRollDiceClick();
+        initializeGameBoard();
         scaleGestureDetector = new ScaleGestureDetector(requireContext(), new ScaleListener());
         initalizePlayerHomePositions(Game.INSTANCE.players());
+       // displayImageViews(imageViews);
+
 
         return view;
     }
@@ -238,7 +279,41 @@ public class GameBoardFragment extends Fragment {
         btnYellowGoal3 = view.findViewById(R.id.btnGoalYellow3);
         btnYellowGoal4 = view.findViewById(R.id.btnGoalYellow4);
 
-
+        boardField0= view.findViewById(R.id.gameboardpos0);
+        boardField1= view.findViewById(R.id.gameboardpos1);
+        boardField2= view.findViewById(R.id.gameboardpos2);
+        boardField3= view.findViewById(R.id.gameboardpos3);
+        boardField4= view.findViewById(R.id.gameboardpos4);
+        boardField5= view.findViewById(R.id.gameboardpos5);
+        boardField6= view.findViewById(R.id.gameboardpos6);
+        boardField7= view.findViewById(R.id.gameboardpos7);
+        boardField8= view.findViewById(R.id.gameboardpos8);
+        boardField9= view.findViewById(R.id.gameboardpos9);
+        boardField10= view.findViewById(R.id.gameboardpos10);
+        boardField11= view.findViewById(R.id.gameboardpos11);
+        boardField12= view.findViewById(R.id.gameboardpos12);
+        boardField13= view.findViewById(R.id.gameboardpos13);
+        boardField14= view.findViewById(R.id.gameboardpos14);
+        boardField15= view.findViewById(R.id.gameboardpos15);
+        boardField16= view.findViewById(R.id.gameboardpos16);
+        boardField17= view.findViewById(R.id.gameboardpos17);
+        boardField18= view.findViewById(R.id.gameboardpos18);
+        boardField19= view.findViewById(R.id.gameboardpos19);
+        boardField20= view.findViewById(R.id.gameboardpos20);
+        boardField21= view.findViewById(R.id.gameboardpos21);
+        boardField22= view.findViewById(R.id.gameboardpos22);
+        boardField23= view.findViewById(R.id.gameboardpos23);
+        boardField24= view.findViewById(R.id.gameboardpos24);
+        boardField25= view.findViewById(R.id.gameboardpos25);
+        boardField26= view.findViewById(R.id.gameboardpos26);
+        boardField27= view.findViewById(R.id.gameboardpos27);
+        boardField28= view.findViewById(R.id.gameboardpos28);
+        boardField29= view.findViewById(R.id.gameboardpos29);
+        boardField30= view.findViewById(R.id.gameboardpos30);
+        boardField31= view.findViewById(R.id.gameboardpos31);
+        boardField32= view.findViewById(R.id.gameboardpos32);
+        boardField33= view.findViewById(R.id.gameboardpos33);
+        boardField34= view.findViewById(R.id.gameboardpos34);
     }
 
     private void initalizePlayerHomePositions(SortedSet<at.aau.models.Player> players) {
@@ -287,7 +362,81 @@ public class GameBoardFragment extends Fragment {
             }
         }
     }
+    private void initializeGameBoard(){
+        ArrayList<ImageView> listView = new ArrayList<>();
+        boardField0.setImageResource(R.drawable.playericon);
+        boardField1.setImageResource(R.drawable.playericon);
+        boardField2.setImageResource(R.drawable.playericon);
+        boardField3.setImageResource(R.drawable.playericon);
+        boardField4.setImageResource(R.drawable.playericon);
+        boardField5.setImageResource(R.drawable.playericon);
+        boardField6.setImageResource(R.drawable.playericon);
+        boardField7.setImageResource(R.drawable.playericon);
+        boardField8.setImageResource(R.drawable.playericon);
+        boardField9.setImageResource(R.drawable.playericon);
+        boardField10.setImageResource(R.drawable.playericon);
+        boardField11.setImageResource(R.drawable.playericon);
+        boardField12.setImageResource(R.drawable.playericon);
+        boardField13.setImageResource(R.drawable.playericon);
+        boardField14.setImageResource(R.drawable.playericon);
+        boardField15.setImageResource(R.drawable.playericon);
+        boardField16.setImageResource(R.drawable.playericon);
+        boardField17.setImageResource(R.drawable.playericon);
+        boardField18.setImageResource(R.drawable.playericon);
+        boardField19.setImageResource(R.drawable.playericon);
+        boardField20.setImageResource(R.drawable.playericon);
+        boardField21.setImageResource(R.drawable.playericon);
+        boardField22.setImageResource(R.drawable.playericon);
+        boardField23.setImageResource(R.drawable.playericon);
+        boardField24.setImageResource(R.drawable.playericon);
+        boardField25.setImageResource(R.drawable.playericon);
+        boardField26.setImageResource(R.drawable.playericon);
+        boardField27.setImageResource(R.drawable.playericon);
+        boardField28.setImageResource(R.drawable.playericon);
+        boardField29.setImageResource(R.drawable.playericon);
+        boardField30.setImageResource(R.drawable.playericon);
+        boardField31.setImageResource(R.drawable.playericon);
+        boardField32.setImageResource(R.drawable.playericon);
+        boardField33.setImageResource(R.drawable.playericon);
+        boardField34.setImageResource(R.drawable.playericon);
 
+        listView.add(boardField0);
+        listView.add(boardField1);
+        listView.add(boardField2);
+        listView.add(boardField3);
+        listView.add(boardField4);
+        listView.add(boardField5);
+        listView.add(boardField6);
+        listView.add(boardField7);
+        listView.add(boardField8);
+        listView.add(boardField9);
+        listView.add(boardField10);
+        listView.add(boardField11);
+        listView.add(boardField12);
+        listView.add(boardField13);
+        listView.add(boardField14);
+        listView.add(boardField15);
+        listView.add(boardField16);
+        listView.add(boardField17);
+        listView.add(boardField18);
+        listView.add(boardField19);
+        listView.add(boardField20);
+        listView.add(boardField21);
+        listView.add(boardField22);
+        listView.add(boardField23);
+        listView.add(boardField24);
+        listView.add(boardField25);
+        listView.add(boardField26);
+        listView.add(boardField27);
+        listView.add(boardField28);
+        listView.add(boardField29);
+        listView.add(boardField30);
+        listView.add(boardField31);
+        listView.add(boardField32);
+        listView.add(boardField33);
+        listView.add(boardField34);
+
+    }
 
 
 
@@ -298,7 +447,25 @@ public class GameBoardFragment extends Fragment {
         fragmentTransaction.add(R.id.gridLayoutGameBoard, diceFragment);
         fragmentTransaction.commit();
     }
+private List<ImageView> findImageViewByID(int count) {
+    List<ImageView> imageViews = new ArrayList<>();
+    Resources res = getResources();
+    String packageName = requireContext().getPackageName();
+    for (int i = 1; i <= count; i++) {
+        int id = res.getIdentifier("gameboard" + i, "id", packageName);
+        ImageView imageView = requireView().findViewById(id);
+        imageViews.add(imageView);
+    }
+    return imageViews;
+}
 
+    private void displayImageViews(List<ImageView> imageViews) {
+        for (ImageView imageView : imageViews) {
+            // For example, you can set a drawable resource as the image
+            imageView.setImageResource(R.drawable.herb);
+
+        }
+    }
 
 /*
 //necessary in Sprint 2
